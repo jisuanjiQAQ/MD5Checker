907d52cc2b1ef3de07ea8370f23a502a0654cef9
computerqwq <87743828+jisuanjiQAQ@users.noreply.github.com>
2025-03-20 20:54:26 +0800

    Delete man directory

--- man/pro.cmd
+++
@@ -1,7 +0,0 @@
-
-@echo off
-rem This script was created by Nuitka to execute 'pro.exe' with Python DLL being found.
-set PATH=c:\users\computer\appdata\local\programs\python\python38;%PATH%
-set PYTHONHOME=C:\Users\computer\AppData\Local\Programs\Python\Python38
-set NUITKA_PYTHONPATH=F:\Ð¡³ÌÐò;C:\Users\computer\AppData\Local\Programs\Python\Python38\DLLs;C:\Users\computer\AppData\Local\Programs\Python\Python38\lib;C:\Users\computer\AppData\Local\Programs\Python\Python38;C:\Users\computer\AppData\Local\Programs\Python\Python38\lib\site-packages
-"%~dp0pro.exe" %*
